abe6507230989717fb7dc4927b9784bb266e6bb4
Daniel151505 <daniel150520011@gmail.com>
2022-09-05 17:02:42 -0500

    Project Start

---
+++ src/TodoButton/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './TodoButton.css';
+
+function TodoButton(props) {
+    const onClickButton = () => {
+        props.setOpenModal(prevState => !prevState);
+    };
+
+  return (
+    <button 
+        className="CreateTodoButton"
+        onClick={onClickButton}
+    >
+        +
+    </button>
+  );
+}
+
+export { TodoButton };
+
+
